a36d747d1c80cf1bf60a988c4e154cd5522abf23
maniasso <43616714+maniasso@users.noreply.github.com>
2022-07-11 10:33:14 -0300

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -f /firstrun ]; then
+        # remote syslog server to docker host
+        SYSLOG=`netstat -rn|grep ^0.0.0.0|awk '{print $2}'`
+        echo "*.* @$SYSLOG" >> /etc/rsyslog.conf
+
+        # Start syslog server to see something
+        # /usr/sbin/rsyslogd
+
+        echo "Running for first time.. need to configure..."
+
+        # Generate Host keys
+        ssh-keygen -A
+
+        if [[ -z "${TIMEZONE}" ]]; then
+                # set default TZ to London, enable TZ change via GUI
+                TIMEZONE="Europe/London"
+        fi
+        echo "${TIMEZONE}" > /etc/timezone
+        chmod 666 /etc/timezone
+        ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+
+
+        # initialize lpar2rrd's crontab
+        crontab -u root /var/spool/cron/crontabs/root
+
+        # clean up
+        rm /firstrun
+fi
+
+# Sometimes with un unclean exit the rsyslog pid doesn't get removed and refuses to start
+if [ -f /var/run/rsyslogd.pid ]; then
+        rm /var/run/rsyslogd.pid
+fi
+
+# Start supervisor to start the services
+/usr/bin/supervisord -c /etc/supervisord.conf -l /var/log/supervisor.log -j /var/run/supervisord.pid
